5e9e541fabd3396149619f6f4702d02b6415ab96
Ben Goeters <0lafethehorsescrub@gmail.com>
2021-11-18 18:05:55 -0500

    more fixes coming

--- spec/features/images/01_band_can_upload_image_spec.rb
+++ spec/features/images/01_band_can_upload_image_spec.rb
@@ -1,7 +1,16 @@
 require "rails_helper"
 
 feature "band photo" do
+  let!(:test_user) {User.create!(email: "test_user@mail.com", password: "password", username: "test_user_2", first_name: "test", role: "admin")}
   scenario "user uploads a band profile photo" do
+    visit "/"
+    click_link "Sign In"
+
+    fill_in 'Email', with: test_user.email
+    fill_in 'Password', with: test_user.password
+
+    click_button "Log in"
+
     visit new_band_path
 
     fill_in "Band Name", with: "Flamey-Os"
