2012aa2ea9f4a68c24bc64182cbdc339a838282f
Ben Goeters <0lafethehorsescrub@gmail.com>
2021-11-18 18:21:51 -0500

    fixed the fixes

--- app/javascript/react/components/bands/ReviewTiles.js
+++ app/javascript/react/components/bands/ReviewTiles.js
@@ -2,13 +2,18 @@ import React from 'react'
 
 const ReviewTiles = (props) => {
   const { review, user } = props
-
   const formattedRating = `${"★".repeat(review.rating)}${"☆".repeat(5-review.rating)}`
+
+  let text
+
+  if (user.role == "admin"){
+    text = "ADMIN"
+  }
 
   return (
     <div className="review-tile cell small-8">
       <div className="review">
-        <h2>{user.username}</h2>
+        <h2 className={text}>{user.username}</h2>
         <p>{formattedRating}</p>
       </div>
       <h3>{review.body}</h3>
