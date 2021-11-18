b39e415792d6e8532b26cc92332c04641ec2110a
Josh Mora <joshrmora@gmail.com>
2021-11-18 14:27:46 -0500

    added radio buttons to review ratings

--- app/javascript/react/components/bands/ReviewsForm.js
+++ app/javascript/react/components/bands/ReviewsForm.js
@@ -9,24 +9,40 @@ const ReviewForm = (props) => {
     setFormData({
       ...formData,
       [event.currentTarget.name]: newValue
-
     })
-
   }
+
+  const handleButtons = (event) => {
+    event.preventDefault()
+
+    setFormData({
+      ...formData,
+      "rating": parseInt(event.currentTarget.name)
+    })
+  }
+
+  const numbers = [0, 1, 2, 3, 4, 5]
+
+  const ratingButtons = (numbers).map(number => {
+    return (
+      <div key={number}>
+        <input type="radio" name={number} id={number} onChange={handleButtons} value={number}/>
+        <label htmlFor={number}>{number}</label>
+      </div>
+    )
+  })
+
   const handleSubmit = (event) => {
     event.preventDefault()
     props.addNewReview(formData)
   }
+
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="rating">Rating</label>
-      <input 
-      type="text"
-      name="rating"
-      id="rating"
-      onChange={handleChange}
-      value={formData.rating}
-      />
+
+      {ratingButtons}
+
       <label htmlFor="body">Review</label>
       <input 
       type="text"
